4f223624600bb2469b6727ddc74d6a1dbcaf25ea
citra-rahman <citrapuspita.r@gmail.com>
2024-11-12 22:55:10 +0300

    Add date validation for birth and death time

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,10 @@ class Member < ApplicationRecord
   belongs_to :family
 
   enum :gender, [ :male, :female ]
+
+  validate :death_time_validation
+
+  def death_time_validation
+    errors.add(:base, "Birth date can't greater than death date") if birth_date > death_date
+  end
 end
